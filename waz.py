6021f2f604119ea94180cbb7b6f2e07a58b3eb37
No0myy <98463211+No0myy@users.noreply.github.com>
2022-03-23 13:05:43 +0100

    Add files via upload

--- waz.py
+++ waz.py
@@ -1,45 +1,64 @@
 import pygame
-
 class Waz():
-    #konstruktor klasy
-    #tworzy podstawowe elementy klasy podczas wywołania jej
+    #konstruktor klasy - uruchamia się podczas utworzenia obiektu
     def __init__(self):
-        self.__pozycja=[(100,100)]
-        self.dlugoscWeza=1
+        self.dlugosc=1
+        self.pozycja=[(160,160)]
         self.punkty=0
-        self.kierunek=(0,-1)
-    def getPosition(self):
-        return self.__pozycja[-1]
-    def setPosition(self,x,y):
-        self.__pozycja[-1]=(x,y)
-    def setKierunek(self,direction):
-            self.kierunek=direction
-    def ruch(self):
-        #obliczenie nowej pozycji
-        ostatniaPozycja=self.__pozycja[-1]
-        x=ostatniaPozycja[0]+20*self.kierunek[0]
-        y=ostatniaPozycja[1]+20*self.kierunek[1]
-         #sprawdzenie czy waz nie zjada siebie
-        for location in self.__pozycja[::]:
-            if x==location[0] and y==location[1]:
-                    self.__pozycja=[(x,y)]
-                    self.dlugoscWeza=1
-                    self.punkty=0
-        #dodanie nowej pozycji weza
-        self.__pozycja.append((x,y))
-         #nie usuwamy pozycji gdy waz zjadl jablko
-        if len(self.__pozycja)>self.dlugoscWeza:
-            del self.__pozycja[0]
-    #funkcja zjadania jablka
-    def zjadanie(self):
-        #self.dlugoscWeza=self.dlugoscWeza+1
-        self.dlugoscWeza+=1
-        self.punkty+=1
-    #funkcja rysująca węża
-    #jako parametry wywołania używa self - samej siebie oraz OknoGry- tam gdzie będziemy rysować węża
-    def rysowanie(self, OknoGry):
-        #rysowanie węża z pozycji
-        for poz in self.__pozycja[::-1]:
-            r=pygame.Rect((poz[0],poz[1]),(20,20))
-            pygame.draw.rect(OknoGry,(self.kolor),r)
+        self.kierunek=(0,1)
+    #ustalanie kierunku
+    def setDirection(self,direction):
+        self.kierunek=direction
+    #zwrócenie pozycji głowy
+    def getHeadPosition(self):
+        return self.pozycja[-1]
+    #dodanie nowej pozycj węża
+    def addPosition(self,x,y):
+        self.pozycja.append((x,y))
+    #wykonanie ruchu przez węża
+    def snakeMove(self):
+        #ostatnie pozycja głowy
+        ostatniaPozycja=self.pozycja[-1]
+        x=ostatniaPozycja[0]+20*kierunek[0]
+        y=ostatniaPozycja[1]+20*kierunek[1]
+        #sprawdzanie krawedzi
+        noweWspolrzedne=self.checkBorder(x,y)
+        #sprawdzenie czy wąż nie zjadł sam siebie
+        for czesciCiala in self.pozycja[::]:
+            if czesciCiala[0]==noweWspolrzedne[0] and czesciCiala[1]==noweWspolrzedne[1]:
+                self.dlugosc=1
+                self.punkty=0
             
+        #dodanie nowej pozycji
+        if self.dlugosc==1:
+            self.pozycja=[noweWspolrzedne]
+        else:
+            self.addPosition(noweWspolrzedne[0],noweWspolrzedne[1])
+        #jeżeli wąż jest za dlugi, usunięcie pierwszej pozycji
+        if len(self.pozycja)>self.dlugosc:
+            del self.pozycja[0]
+    #zwiększanie paramtrów w momencie zjadania jablka
+    def snakeEat(self):
+        #self.dlugosc=self.dlugosc+1
+        self.dlugosc+=1
+        self.punkty+=1
+    #rysowanie węża
+    def drawSnake(self, Oknogry):
+        for position in self.pozycja[::-1]:
+            kwadrat = pygame.Rect((position[0],position[1]),(20,20))
+            pygame.draw.rect(Oknogry,(255,0,0),kwadrat)
+    #sprawdzanie krawedzi okna
+    def checkBorder(self,zmienna1,zmienna2):
+        #przejscie prawa
+        if zmienna1>300:
+            zmienna1=0
+        #przejscie dol
+        if zmienna2>300:
+            zmienna2=0
+        #przejscie lewa
+        if zmienna1<0:
+            zmienna1=300
+        #przejscie gora
+        if zmienna2<0:
+            zmienna2=300
+        return (zmienna1,zmienna2)
